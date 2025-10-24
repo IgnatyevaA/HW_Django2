8ff0f6d2740a95fb6c536ac1a2cd2d4a9411e959
My name <shyra9072@gmail.com>
2025-10-24 23:03:34 +0300

    Third commit
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   .env.example
    	new file:   catalog/managers/commands/add_products.py
    	modified:   catalog/models.py
    	new file:   catalog_fixture.json
    	modified:   config/settings.py
    	modified:   config/urls.py
    	modified:   requirements.txt
    	new file:   screenshots/1.png
    	new file:   screenshots/2.png
    	new file:   screenshots/3.png
    	new file:   screenshots/4.png

--- catalog/models.py
+++ catalog/models.py
@@ -1,24 +1,47 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Имя товара",
+        help_text="Введите наиманование товара",
+    )
+    description = models.TextField(
+        verbose_name="Описание товара", help_text="Введите описание товара"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+
 class Product(models.Model):
     name = models.CharField(
-        max_length=100, 
-        verbose_name="Имя товара", 
-        help_text="Введите наиманование товара")
+        max_length=100,
+        verbose_name="Имя товара",
+        help_text="Введите наиманование товара",
+    )
     description = models.TextField(
-        verbose_name="Описание товара", 
-        help_text="Введите описание товара")
+        verbose_name="Описание товара", help_text="Введите описание товара"
+    )
     photo = models.ImageField(
-        upload_to="catalog/photo", 
-        blank=True, null=True, 
-        verbose_name="Фото", 
-        help_text="Загрузите фото товара")
-    category = models.CharField(
-        max_length=100, verbose_name="Название категории товара", 
-        help_text="Введите категорию товара")
+        upload_to="catalog/photo",
+        blank=True,
+        null=True,
+        verbose_name="Фото",
+        help_text="Загрузите фото товара",
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="products"
+    )
     price = models.IntegerField(
-        verbose_name="Цена товара", help_text="Введите цену за товар")
+        verbose_name="Цена товара", help_text="Введите цену за товар"
+    )
     created_at = models.DateField(
         verbose_name="Дата создания товара",
         help_text="Введите дату создания товара",
@@ -32,23 +55,6 @@ class Product(models.Model):
         return self.name
 
     class Meta:
-        verbose_name="Товар"
+        verbose_name = "Товар"
         verbose_name_plural = "Товары"
         ordering = ["name", "category"]
-
-
-class Category(models.Model):
-    name = models.CharField(
-        max_length=100, verbose_name="Имя товара", 
-        help_text="Введите наиманование товара")
-    description = models.TextField(
-        verbose_name="Описание товара", 
-        help_text="Введите описание товара")
-    
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name="Категория"
-        verbose_name_plural = "Категории"
-        ordering = ["name"]
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +78,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Sasha2008@',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT', default='5432'),
     }
 }
 

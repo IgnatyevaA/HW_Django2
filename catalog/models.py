358fc6b8bc34a345297438e7e61c4596be223a7a
IgnatyevaA <shyra9072@gmail.com>
2025-10-24 12:01:37 +0300

    Second commit

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100, 
+        verbose_name="Имя товара", 
+        help_text="Введите наиманование товара")
+    description = models.TextField(
+        verbose_name="Описание товара", 
+        help_text="Введите описание товара")
+    photo = models.ImageField(
+        upload_to="catalog/photo", 
+        blank=True, null=True, 
+        verbose_name="Фото", 
+        help_text="Загрузите фото товара")
+    category = models.CharField(
+        max_length=100, verbose_name="Название категории товара", 
+        help_text="Введите категорию товара")
+    price = models.IntegerField(
+        verbose_name="Цена товара", help_text="Введите цену за товар")
+    created_at = models.DateField(
+        verbose_name="Дата создания товара",
+        help_text="Введите дату создания товара",
+    )
+    updated_at = models.DateField(
+        verbose_name="Дата последнего изменения товара",
+        help_text="Введите дату последнего изменения товара",
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name="Товар"
+        verbose_name_plural = "Товары"
+        ordering = ["name", "category"]
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100, verbose_name="Имя товара", 
+        help_text="Введите наиманование товара")
+    description = models.TextField(
+        verbose_name="Описание товара", 
+        help_text="Введите описание товара")
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name="Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
